cf722f5682195f28cd0fc0d1ec52154f2d46cfbb
Shadowgraph <litvin4chuk@mail.ru>
2025-06-15 19:17:12 +0500

    Основные проектные файлы

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HomeStart from "./pages/FirstPage";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HomeStart />
+    </>
+  );
+}
+
+export default App;
