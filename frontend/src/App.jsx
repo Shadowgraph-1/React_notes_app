6e76a65f9c092251a692b70b8c01338def9d9e8d
Shadowgraph <litvin4chuk@mail.ru>
2025-06-18 19:54:14 +0500

    Правки в app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HomeStart from "./pages/FirstPage";
 import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "../src/components/ToastStyles.css";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('access_token'));
+
+  useEffect(() => {
+    const token = localStorage.getItem('access_token');
+    setIsAuthenticated(!!token);
+  }, []);
+
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
       <Header />
-      <HomeStart />
+      <HomeStart isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
     </>
   );
 }
